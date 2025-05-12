ad56a0eefb1a8ad2ad64401231c26e22c635966f
sooraj002 <soorajnambiar8@gmail.com>
2025-05-13 01:04:57 +0530

    able to downlaod playlists

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import yt_dlp
 
-url = input("Enter the YouTube video URL: ").strip()
+url = input("Enter the YouTube playlist or video URL: ").strip()
 
 ydl_opts = {
     'format': 'best',
-    'outtmpl': './output/%(title)s.%(ext)s',
+    'outtmpl': './output/%(playlist_title)s/%(playlist_index)s - %(title)s.%(ext)s',
+    'ignoreerrors': True,  # Skip videos that fail to download
+    'noplaylist': False    # Enable full playlist download
 }
 
 with yt_dlp.YoutubeDL(ydl_opts) as ydl:
